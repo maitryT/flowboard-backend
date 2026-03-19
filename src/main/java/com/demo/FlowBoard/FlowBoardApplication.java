bd423dcf84eb3c78f70474a9ac1eed06f9c8911b
Maitry Thakkar <maitrythakkar08@gmail.com>
2026-03-19 16:17:30 +0530

    init commit

---
+++ src/main/java/com/demo/FlowBoard/FlowBoardApplication.java
@@ -0,0 +1,13 @@
+package com.demo.FlowBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowBoardApplication.class, args);
+	}
+
+}

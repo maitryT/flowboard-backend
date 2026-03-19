160f2b7b212b048911264ca07d428e1499a72fb4
Maitry Thakkar <maitrythakkar08@gmail.com>
2026-03-19 16:47:58 +0530

    new commit

---
+++ src/main/java/com/demo/flowboard_backend/FlowboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo.flowboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowboardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/flowboard_backend/FlowboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.flowboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
